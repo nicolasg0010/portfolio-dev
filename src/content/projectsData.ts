94733e586c6a47fec3d27a1bc07cf9f01306c4b8
Nicolas Garcia <nicolasg0010@gmail.com>
2023-01-05 09:00:14 -0300

    eknows deploy url

--- src/content/projectsData.ts
+++ src/content/projectsData.ts
@@ -14,7 +14,7 @@ export const projectsData = [
       'https://github.com/eknows-ecommerce/pf-front',
       'https://github.com/eknows-ecommerce/pf-back',
     ],
-    previewURL: 'https://e-knows.herokuapp.com/',
+    previewURL: 'https://eknows-ecomm.vercel.app',
     tools: [
       'React',
       'Redux',
